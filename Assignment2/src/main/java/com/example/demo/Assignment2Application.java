a507da1297585fe2cb61cbae96e869ea90b04154
gaduprasad <gaduprasad@INBAWN166712.ad.harman.com>
2022-05-09 14:20:53 +0530

    SpringBoot

---
+++ Assignment2/src/main/java/com/example/demo/Assignment2Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment2Application.class, args);
+		System.out.println("Hello!, Welcome to Spring Boot ");
+	}
+
+}
